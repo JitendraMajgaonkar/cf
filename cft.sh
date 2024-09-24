75142ec2bfe681418bfbfe04a9a278fde362b9b7
JitendraMajgaonkar <53226406+JitendraMajgaonkar@users.noreply.github.com>
2024-09-24 21:55:03 +0200

    Create cft.sh

---
+++ cft.sh
@@ -0,0 +1,3 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+sudo dpkg -i cloudflared.deb && 
+sudo cloudflared service install eyJhIjoiNDMyNDc1NzBkZmI1NWU0NDI2MjA3YWQyOWRmN2VjODAiLCJ0IjoiNTY2YzM1NGEtYjc0MC00NjFmLTg1NGItOTI1NThlZjkzOWE4IiwicyI6Ik5UY3dNMkk1TlRrdE1ESXpaQzAwWldRMUxUbGtNRGt0WlRFNE9EQTJaR05pTWpSayJ9
