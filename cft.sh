857663c43f0b26042c17936fe07c3fdce26229d1
JitendraMajgaonkar <53226406+JitendraMajgaonkar@users.noreply.github.com>
2024-09-24 22:43:06 +0200

    Update cft.sh

--- cft.sh
+++ cft.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Download the latest cloudflared .deb package
-curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
 
-# Install the downloaded package
-sudo dpkg -i cloudflared.deb
+sudo dpkg -i cloudflared.deb && 
 
-# Install the cloudflared service with the given token
-sudo cloudflared service install eyJhIjoiNDMyNDc1NzBkZmI1NWU0NDI2MjA3YWQyOWRmN2VjODAiLCJ0IjoiNTY2YzM1NGEtYjc0MC00NjFmLTg1NGItOTI1NThlZjkzOWE4IiwicyI6Ik5UY3dNMkk1TlRrdE1ESXpaQzAwWldRMUxUbGtNRGt0WlRFNE9EQTJaR05pTWpSayJ9
+sudo cloudflared service install eyJhIjoiNDMyNDc1NzBkZmI1NWU0NDI2MjA3YWQyOWRmN2VjODAiLCJ0IjoiNTY2YzM1NGEtYjc0MC00NjFmLTg1NGItOTI1NThlZjkzOWE4IiwicyI6Ik5tUmtaV0ZqWlRndE1tSmhOaTAwTXpjMUxXSXpaVEV0T1dZMk5HVXpOVFUxWVRndyJ9
